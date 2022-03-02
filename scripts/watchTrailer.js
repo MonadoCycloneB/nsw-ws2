57bc8c79442a531274f1f28952ac0d9470d4a47b
Dolev Irron <dolev.irron@gmail.com>
2022-03-02 17:44:31 +0200

    begun adjusting trailer responsiveness

--- scripts/watchTrailer.js
+++ scripts/watchTrailer.js
@@ -1,12 +1,16 @@
 "use strict";
 
 const trailerLink = document.querySelector(".trailer-link");
-const trailerFrame = trailerLink.querySelector("iframe");
+const trailerFrame = trailerLink.querySelector(".trailer-container");
 const scrShot = trailerLink.querySelector("img");
 const strong = trailerLink.querySelector("strong");
+const frameContainer = document.querySelector(".frame-container");
 
 trailerLink.addEventListener("click", function () {
 	scrShot.style.opacity = "0";
 	strong.style.opacity = "0";
 	trailerFrame.style.display = "block";
+	frameContainer.classList.add("trailer-hide");
+	scrShot.classList.add("trailer-hide");
+	strong.classList.add("trailer-hide");
 });
